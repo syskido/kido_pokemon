9b0db713ef218794c419384f9ca4a03858764253
kido <61651321+syskido@users.noreply.github.com>
2024-07-28 19:21:32 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,99 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="YUN-Kido 포켓몬 도감",
+    page_icon="./images/monsterball.png"
+)
+st.title("streamlit **[나령 윤기도]** 포켓몬 도감")
+st.markdown("**포켓몬**을 하나씩 추가해서 도감을 채워보세요!")
+
+type_emoji_dict = {
+    "노말": "⚪",
+    "격투": "✊",
+    "비행": "🕊",
+    "독": "☠️",
+    "땅": "🌋",
+    "바위": "🪨",
+    "벌레": "🐛",
+    "고스트": "👻",
+    "강철": "🤖",
+    "불꽃": "🔥",
+    "물": "💧",
+    "풀": "🍃",
+    "전기": "⚡",
+    "에스퍼": "🔮",
+    "얼음": "❄️",
+    "드래곤": "🐲",
+    "악": "😈",
+    "페어리": "🧚"
+}
+
+pokemons = [
+    {
+        "name": "피카츄",
+        "types": ["전기"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/pikachu.webp"
+    },
+    {
+        "name": "누오",
+        "types": ["물", "땅"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/nuo.webp",
+    },
+    {
+        "name": "갸라도스",
+        "types": ["물", "비행"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/garados.webp",
+    },
+    {
+        "name": "개굴닌자",
+        "types": ["물", "악"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/frogninja.webp"
+    },
+    {
+        "name": "루카리오",
+        "types": ["격투", "강철"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/lukario.webp"
+    },
+    {
+        "name": "에이스번",
+        "types": ["불꽃"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/acebun.webp"
+    },
+]
+
+
+with st.form(key="form"):
+    name = st.text_input(label="포켓몬 이름")
+    types = st.multiselect(
+        label="포켓몬 속성",
+        options=list(type_emoji_dict.keys()),
+        max_selections=2
+    )
+    
+    submit = st.form_submit_button(label="Submit")
+
+
+
+
+for i in range(0, len(pokemons), 3):
+    row_pokemons = pokemons[i:i+3]
+    cols = st.columns(3)
+    for j in range(len(row_pokemons)):
+        with cols[j]:
+            pokemon = row_pokemons[j]
+            with st.expander(label=f"**{i+j+1}. {pokemon["name"]}**", expanded=True):
+                st.image(pokemon["image_url"])
+                emoji_types = [f"{type_emoji_dict[x]} {x}" for x in pokemon["types"]]
+                st.subheader(" / ".join(emoji_types))
+
+
+
+
+
+
+
+
+
+
+
+
